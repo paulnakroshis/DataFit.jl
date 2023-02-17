c42dc45c68a9dc0c60698ca31cb49181588f1697
Paul Nakroshis <pauln@maine.edu>
2023-02-17 12:09:34 -0500

    added function to test a time series.

--- src/fit.jl
+++ src/fit.jl
@@ -5,6 +5,30 @@ function fit_data(model, tdata, ydata, p0)
     return fit_params, σ # returns a tuple of fit parameters and their standard deviations
 end
 
+
+# analyze spacing bewteen elements of a Vector
+function spacing_info(v::Vector, full_stats::Bool=false)
+    Δt =v[2:end] - v[1:end-1]
+    if any(Δt .<= 0)
+        error("Vector must be strictly increasing")
+    end
+    Δtmin = minimum(Δt)
+    Δtmax = maximum(Δt)
+    Δtmean = mean(Δt)
+    Δtstd = std(Δt)
+    if full_stats
+        Δtmin = minimum(Δt)
+        Δtmax = maximum(Δt)
+        Δtmean = mean(Δt)
+        Δtstd = std(Δt)
+        return Δtmin, Δtmax, Δtmean, Δtstd
+    else
+        return Δtmin
+    end
+end
+
+
+
 function bootstrap_fit(model, tdata, δt, Σt, ydata, δy, Σy, p0, m_samples)
     fit_params, σ = fit_data(model, tdata, ydata, p0)
     fit_params_array = zeros(length(fit_params), m_samples)
