f673d7b998953bb729c875864926024a8664030d
Paul Nakroshis <pauln@maine.edu>
2023-03-13 15:01:04 -0400

    Update DataFit.jl

--- src/DataFit.jl
+++ src/DataFit.jl
@@ -1,6 +1,9 @@
 module DataFit
 
-# Write your package code here.
-x = 2.0
+export fit_data,  spacing_info, bootstrap_fit
+include("fit.jl")
+
+export  Δp, uncertainty
+include("sigma_model.jl")
 
 end
